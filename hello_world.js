7059bf153c233c039381517bce650d21604bef17
Tanisi <66327432+CodeHubz@users.noreply.github.com>
2021-05-10 19:16:15 -0400

    Add files via upload

---
+++ hello_world.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+let name= ("Hello Tanisi, How are you?");
+console.log(name);
+console.log("Good Night JavaScript! ")
+                                                                 
